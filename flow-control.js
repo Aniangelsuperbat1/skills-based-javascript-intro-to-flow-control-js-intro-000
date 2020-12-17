678fdd6bf8a5e77d88956bd5bd573e6581646e3d
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-17 17:28:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return conditionToTest ? valueToBeReturnedIfTrue : valueToBeReturnedIfFalse
+  return (age >= 13 && age <=19) ? "You are a teenager!" : "You are not a teenager"
 }
 
 function switchAge(age) {
